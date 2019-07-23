95d6de2bfe58581e0c229ea2582cd2b6990e7dd6
felexkemboi <felokemboi10@gmail.com>
2019-07-23 17:45:19 +0300

    add danger button

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,17 +6,36 @@
    <h1 class="title">Create a new Project</h1>
   <form method="POST" action="/projects" style="margin-bottom: 1em">
     @csrf
-    <div>
-      <input type="text" class="input" name="title" placeholder=" Project Title" required >
+    <div class="field">
+      <label class="label" for="title">Title</label>
+      <div class="control">
+        <input type="text" class="input" name="title" placeholder=" Project Title" required>
+      </div>
     </div>
-    &nbsp &nbsp
-    <div>
+     &nbsp &nbsp
+    <div class="field">
+      <label class="label" for="title">Project Description</label>
+      <div class="control">
         <textarea name="description" class="textarea" placeholder="Project Description" required></textarea>
+      </div>
     </div>
     &nbsp &nbsp
     <div class="control">
       <button type="submit">Add Project</button>
     </div>
+    
+    &nbsp &nbsp
+
+    @if ($errors -> any())
+      <div class="notification is-danger">
+        <ul>
+          @foreach ($errors -> all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif 
+
   </form>
 
 @endsection

7242be632767c521d7fbe6ddc945e57099877fe1
felexkemboi <felokemboi10@gmail.com>
2019-07-24 08:59:46 +0300

    add check box to tasks of efvery project

---
+++ database/migrations/2019_07_23_161958_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table -> boolean('completed') -> default(false);
+            $table->unsignedInteger('project_id'); //it can be integer as well but use unsigned for + numbers only
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,5 +7,20 @@
     <div class="content">{{ $project -> description }} </div>
     <button><p><a href="/projects/{{ $project->id }}/edit">Edit</a></p></button> 
     <button><p><a href="/projects">Home</a></p></button>
+
+    @if ($project -> tasks -> count())
+      <div>
+        @foreach(  $project-> tasks as $task)
+        <div>
+          <form>
+            <label class="checkbox" for="completed">
+              <input type="checkbox" name="completed">
+              {{ $task -> description }}
+            </label>
+          </form>
+        </div>
+        @endforeach
+      </div>
+    @endif
 @endsection
 
